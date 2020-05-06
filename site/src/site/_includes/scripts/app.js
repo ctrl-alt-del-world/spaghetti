0a55df0fd9fb2f7728d4500b7dd31949497df901
Kevin Green <kevin@ctrlaltdel.world>
2020-05-06 10:11:55 -0400

    starting to make that spaghetti weird

--- site/src/site/_includes/scripts/app.js
+++ site/src/site/_includes/scripts/app.js
@@ -1,13 +1,15 @@
 import { picoapp } from 'picoapp'
 
 import image from './lib/image.js'
+import theme from './lib/theme.js'
 
 const state = {
   menuOpen: false
 }
 
 const components = {
-  image
+  image,
+  theme
 }
 
 export default picoapp(components, state)
